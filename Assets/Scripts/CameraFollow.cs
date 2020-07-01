c7368d76afd430b245540d650452d205fd438fe8
adamsandlerisfunny <lnicolaidou.katerina@gmail.com>
2020-07-01 20:50:40 +0100

    Third Person Camera + Movement
    Camera basic position and rotation.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target, player;
+    public float rotationSpeed;
+    float mouseX, mouseY;
+
+    void Start()
+    {
+        //Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate(){
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);   
+        player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+    }
+}
